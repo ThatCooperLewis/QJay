5e82d6e575ceeb4f880b845fc973375b256f9763
ThatCooperLewis <cooper.lewis@gatherhere.com>
2020-03-09 21:54:00 -0400

    Add config maps class

--- config_maps.py
+++ config_maps.py
@@ -1,1 +1,19 @@
 # Class for config, allow for `item.config_thing` declarations
+
+from music_objects import Track, Artist, Album
+from logger import Logger
+
+log = Logger('config_maps').info
+
+class SearchConfig():
+
+    def __init__(self, type_str, type_plural_str, typeClass):
+        self.type = type_str
+        self.types = type_plural_str
+        self.typeClass = typeClass
+
+def get_search_maps():
+    track  = SearchConfig('track',  'tracks',  Track)
+    artist = SearchConfig('artist', 'artists', Artist)
+    album  = SearchConfig('album',  'albums',  Album)
+    return (track, artist, album)
